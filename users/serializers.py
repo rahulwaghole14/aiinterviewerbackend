0eab450f5b77634606c5ff1bc29654f52435412b
Bhavana-Munot <115860546+Bhavana-Munot@users.noreply.github.com>
2025-07-23 15:53:29 +0530

    Update serializers.py

--- users/serializers.py
+++ users/serializers.py
@@ -7,12 +7,12 @@ from .models import CustomUser, Role
 
 class RegisterSerializer(serializers.ModelSerializer):
     password = serializers.CharField(write_only=True, required=True, validators=[validate_password])
-    
+
     class Meta:
         model = CustomUser
         fields = ['id', 'username', 'email', 'full_name', 'company_name', 'role', 'password']
         extra_kwargs = {
-            'username': {'read_only': True}  # auto-generated from email
+            'username': {'read_only': True}  # Automatically set to email
         }
 
     def validate_role(self, value):
@@ -26,3 +26,5 @@ class RegisterSerializer(serializers.ModelSerializer):
             return CustomUser.objects.create_user(**validated_data)
         except DjangoValidationError as e:
             raise serializers.ValidationError({'error': e.messages})
+        except Exception as e:
+            raise serializers.ValidationError({'error': str(e)})

191ca6a16888b63d6e39dbb1d388f12fe529cbd0
Bhavana-Munot <115860546+Bhavana-Munot@users.noreply.github.com>
2025-07-28 17:57:56 +0530

    Delete jobs directory

--- jobs/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from .views import JobListCreateView, JobDetailView, JobTitleListView
-
-urlpatterns = [
-    path('', JobListCreateView.as_view(), name='job-list-create'),         # GET, POST
-    path('<int:id>/', JobDetailView.as_view(), name='job-detail'),         # GET, PUT, DELETE
-    path('titles/', JobTitleListView.as_view(), name='job-titles'),        # GET
-]

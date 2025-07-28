2ba6b8ad808032ff36dbc02b8b82c2ee9f5d3ab8
Bhavana-Munot <115860546+Bhavana-Munot@users.noreply.github.com>
2025-07-28 17:56:17 +0530

    Delete candidates directory

--- candidates/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from .views import CandidateListCreateView, CandidateDetailView, CandidateSummaryView
-
-urlpatterns = [
-    path("", CandidateListCreateView.as_view(), name="candidate-list-create"),
-    path("<int:pk>/", CandidateDetailView.as_view(), name="candidate-detail"),  # or <uuid:pk>
-    path("summary/", CandidateSummaryView.as_view(), name="candidate-summary"),
-]

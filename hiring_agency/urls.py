c0078c6501d7c4acbe67cb3eedb83cc88261c01e
Rahul W <rahulwaghole14@gmail.com>
2025-08-01 13:33:10 +0530

    Hiring agency listing issue

--- hiring_agency/urls.py
+++ hiring_agency/urls.py
@@ -1,6 +1,12 @@
 from django.urls import path
 from .views import CreateUserDataView
+from rest_framework.routers import DefaultRouter
+from .views import UserDataViewSet
+
+router = DefaultRouter()
+router.register(r'hiring_agency', UserDataViewSet, basename='hiring_agency')
 
 urlpatterns = [
     path('add_user/', CreateUserDataView.as_view(), name='add_user'),
+    *router.urls,
 ]

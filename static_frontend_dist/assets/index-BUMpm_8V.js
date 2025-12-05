828af11a929627e66c1c0fb864d33b2448c10aa1
Dhananjay9075 <paturkardhananjay4321@gmail.com>
2025-12-05 17:57:17 +0530

    Update frontend build with API URL fix - uses window.location.origin in production

---
+++ static_frontend_dist/assets/index-BUMpm_8V.js
@@ -0,0 +1,1 @@
+import{aC as a,aD as r}from"./index-9DSA42Q2.js";var t=a();const e=r(t);export{e as R,t as r};
--- static_frontend_dist/assets/index-YPs6-RzR.js
+++
@@ -1,1 +0,0 @@
-import{aC as a,aD as r}from"./index-DasKWjZg.js";var t=a();const e=r(t);export{e as R,t as r};
